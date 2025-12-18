1b1c5318c067f13aa365fc64b7f4174f58510faa
Luigi Bonati <lui.bonati@gmail.com>
2025-12-18 13:58:05 +0100

    update mace registry

--- franken/backbones/update_mace_registry.py
+++ franken/backbones/update_mace_registry.py
@@ -10,12 +10,14 @@ import json
 from mace.calculators.foundations_models import mace_mp_urls
 
 # add development ones
-mace_mp_urls.update({
-    "mh-0": "https://github.com/ACEsuit/mace-foundations/releases/download/mace_mh_1/mace-mh-0.model",
-    "mh-1": "https://github.com/ACEsuit/mace-foundations/releases/download/mace_mh_1/mace-mh-1.model",
-    "mace_omol_0_1024": "https://github.com/ACEsuit/mace-foundations/releases/download/mace_omol_0/MACE-omol-0-extra-large-1024.model",
-    "mace_omol_0_4M": "https://github.com/ACEsuit/mace-foundations/releases/download/mace_omol_0/mace-omol-0-extra-large-4M.model"
-})
+mace_mp_urls.update(
+    {
+        "mh-0": "https://github.com/ACEsuit/mace-foundations/releases/download/mace_mh_1/mace-mh-0.model",
+        "mh-1": "https://github.com/ACEsuit/mace-foundations/releases/download/mace_mh_1/mace-mh-1.model",
+        "mace_omol_0_1024": "https://github.com/ACEsuit/mace-foundations/releases/download/mace_omol_0/MACE-omol-0-extra-large-1024.model",
+        "mace_omol_0_4M": "https://github.com/ACEsuit/mace-foundations/releases/download/mace_omol_0/mace-omol-0-extra-large-4M.model",
+    }
+)
 
 # update registry
 with open("registry.json", "r") as f:
@@ -27,10 +29,8 @@ for mace_id, mace_url in mace_mp_urls.items():
         "remote": mace_url,
         "local": "mace/" + os.path.basename(mace_url),
         "implemented": True,
-        "kind": "mace"
+        "kind": "mace",
     }
 
 with open("registry.json", "w") as f:
     json.dump(model_registry, f, indent=2)
-
-    
