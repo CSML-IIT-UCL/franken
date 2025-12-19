882c4ada06bf70d425718d26cbb00c501bdd0d28
Luigi Bonati <lui.bonati@gmail.com>
2025-12-19 01:14:39 +0100

    changed name convention for mace in registry

--- franken/backbones/update_mace_registry.py
+++ franken/backbones/update_mace_registry.py
@@ -1,35 +1,46 @@
 """
 Use the list of released MACE models to populate the model registry.
-See : https://github.com/ACEsuit/mace/blob/main/mace/calculators/foundations_models.py
+Some urls are available also inside the mace package: https://github.com/ACEsuit/mace/blob/main/mace/calculators/foundations_models.py
 """
 
 import os
 import json
 
-# retrieve mp ones from mace
-from mace.calculators.foundations_models import mace_mp_urls
+# retrieve mace_mp models from mace
+# from mace.calculators.foundations_models import mace_mp_urls
 
-# add other mace families (off, mh, omol,...)
-mace_mp_urls.update(
-    {
-        "mh-0": "https://github.com/ACEsuit/mace-foundations/releases/download/mace_mh_1/mace-mh-0.model",
-        "mh-1": "https://github.com/ACEsuit/mace-foundations/releases/download/mace_mh_1/mace-mh-1.model",
-        "mace_omol_0_1024": "https://github.com/ACEsuit/mace-foundations/releases/download/mace_omol_0/MACE-omol-0-extra-large-1024.model",
-        "mace_omol_0_4M": "https://github.com/ACEsuit/mace-foundations/releases/download/mace_omol_0/mace-omol-0-extra-large-4M.model",
-        "off-small": "https://github.com/ACEsuit/mace-off/blob/main/mace_off23/MACE-OFF23_small.model?raw=true",
-        "off-medium": "https://github.com/ACEsuit/mace-off/raw/main/mace_off23/MACE-OFF23_medium.model?raw=true",
-        "off24-medium": "https://github.com/ACEsuit/mace-off/blob/main/mace_off24/MACE-OFF24_medium.model?raw=true",
-        "off-large": "https://github.com/ACEsuit/mace-off/blob/main/mace_off23/MACE-OFF23_large.model?raw=true",
-    }
-)
+mace_urls = {
+    "mace_mp/small": "https://github.com/ACEsuit/mace-mp/releases/download/mace_mp_0/2023-12-10-mace-128-L0_energy_epoch-249.model",
+    "mace_mp/medium": "https://github.com/ACEsuit/mace-mp/releases/download/mace_mp_0/2023-12-03-mace-128-L1_epoch-199.model",
+    "mace_mp/large": "https://github.com/ACEsuit/mace-mp/releases/download/mace_mp_0/MACE_MPtrj_2022.9.model",
+    "mace_mp/small-0b": "https://github.com/ACEsuit/mace-mp/releases/download/mace_mp_0b/mace_agnesi_small.model",
+    "mace_mp/medium-0b": "https://github.com/ACEsuit/mace-mp/releases/download/mace_mp_0b/mace_agnesi_medium.model",
+    "mace_mp/small-0b2": "https://github.com/ACEsuit/mace-mp/releases/download/mace_mp_0b2/mace-small-density-agnesi-stress.model",
+    "mace_mp/medium-0b2": "https://github.com/ACEsuit/mace-mp/releases/download/mace_mp_0b2/mace-medium-density-agnesi-stress.model",
+    "mace_mp/large-0b2": "https://github.com/ACEsuit/mace-mp/releases/download/mace_mp_0b2/mace-large-density-agnesi-stress.model",
+    "mace_mp/medium-0b3": "https://github.com/ACEsuit/mace-mp/releases/download/mace_mp_0b3/mace-mp-0b3-medium.model",
+    "mace_mpa/medium-0": "https://github.com/ACEsuit/mace-mp/releases/download/mace_mpa_0/mace-mpa-0-medium.model",
+    "mace_omat/small-0": "https://github.com/ACEsuit/mace-mp/releases/download/mace_omat_0/mace-omat-0-small.model",
+    "mace_omat/medium-0": "https://github.com/ACEsuit/mace-mp/releases/download/mace_omat_0/mace-omat-0-medium.model",
+    "mace_matpes/pbe-0": "https://github.com/ACEsuit/mace-foundations/releases/download/mace_matpes_0/MACE-matpes-pbe-omat-ft.model",
+    "mace_matpes/r2scan-0": "https://github.com/ACEsuit/mace-foundations/releases/download/mace_matpes_0/MACE-matpes-r2scan-omat-ft.model",
+    "mace_mh/0": "https://github.com/ACEsuit/mace-foundations/releases/download/mace_mh_1/mace-mh-0.model",
+    "mace_mh/1": "https://github.com/ACEsuit/mace-foundations/releases/download/mace_mh_1/mace-mh-1.model",
+    "mace_omol/0_1024": "https://github.com/ACEsuit/mace-foundations/releases/download/mace_omol_0/MACE-omol-0-extra-large-1024.model",
+    "mace_omol/0_4M": "https://github.com/ACEsuit/mace-foundations/releases/download/mace_omol_0/mace-omol-0-extra-large-4M.model",
+    "mace_off/small": "https://github.com/ACEsuit/mace-off/blob/main/mace_off23/MACE-OFF23_small.model?raw=true",
+    "mace_off/medium": "https://github.com/ACEsuit/mace-off/raw/main/mace_off23/MACE-OFF23_medium.model?raw=true",
+    "mace_off/medium24": "https://github.com/ACEsuit/mace-off/blob/main/mace_off24/MACE-OFF24_medium.model?raw=true",
+    "mace_off/large": "https://github.com/ACEsuit/mace-off/blob/main/mace_off23/MACE-OFF23_large.model?raw=true",
+}
 
 # update registry
 with open("registry.json", "r") as f:
     model_registry = json.load(f)
 
-for mace_id, mace_url in mace_mp_urls.items():
+for mace_id, mace_url in mace_urls.items():
     print(mace_id)
-    model_registry[f"mace/{mace_id}"] = {
+    model_registry[f"{mace_id}"] = {
         "remote": mace_url,
         "local": "mace/" + os.path.basename(mace_url),
         "implemented": True,
