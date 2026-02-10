2f8321f67b77e01762c6b3242049c04c2511d2cc
Luigi Bonati <lui.bonati@gmail.com>
2026-02-10 22:15:50 +0100

    remove assert of metric name

--- franken/autotune/script.py
+++ franken/autotune/script.py
@@ -184,11 +184,6 @@ def run_autotune(
     if best_model_selection is None:
         best_model_selection = DEFAULT_BEST_MODEL_SELECTION.copy()
 
-    for metric in best_model_selection:
-        if metric not in metrics:
-            raise ValueError(
-                f"best_model_selection metric '{metric}' is not present in computed metrics {metrics}"
-            )
 
     current_best = BestTrial(None, None)
     rf_param_grid = create_rf_hpsearch_grid(rf_cfg)
