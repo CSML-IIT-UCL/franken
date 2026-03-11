92905918f5da9a88ff5250dbfc7320d8a7f1fd9c
Giacomo Meanti <gmeanti@inria.fr>
2026-03-11 12:20:17 +0100

    Add Metatomic wrapper to support their calculator

---
+++ franken/calculators/metatomic_calc.py
@@ -0,0 +1,179 @@
+import argparse
+import os
+from typing import Dict, List, Optional
+
+import torch
+from metatensor.torch import Labels, TensorBlock, TensorMap
+from metatomic.torch import (
+    AtomisticModel,
+    ModelCapabilities,
+    ModelMetadata,
+    ModelOutput,
+    System,
+)
+
+from franken.backbones.wrappers.base import MetatomicModelWrapper
+from franken.data.base import Configuration
+from franken.rf.model import FrankenPotential
+
+
+class MetatomicFrankenWrapper(torch.nn.Module):
+    def __init__(self, franken_model: FrankenPotential):
+        super().__init__()
+        self.model = franken_model
+
+    def forward(
+        self,
+        systems: List[System],
+        outputs: Dict[str, ModelOutput],
+        selected_atoms: Optional[Labels] = None,
+    ) -> Dict[str, TensorMap]:
+        if list(outputs.keys()) != ["energy"]:
+            raise ValueError(
+                "this model can only compute 'energy', but `outputs` contains other "
+                f"keys: {', '.join(outputs.keys())}"
+            )
+
+        # we don't want to worry about selected_atoms yet
+        if selected_atoms is not None:
+            raise NotImplementedError("selected_atoms is not implemented")
+
+        if outputs["energy"].per_atom:
+            raise NotImplementedError("per atom energy is not implemented")
+
+        energy = torch.zeros((len(systems), 1), dtype=systems[0].positions.dtype)
+        for i, system in enumerate(systems):
+            known_neighbor_lists = system.known_neighbor_lists()
+            if len(known_neighbor_lists) != 1:
+                raise NotImplementedError(
+                    f"Requested {len(known_neighbor_lists)} neighbor lists. We only support 1."
+                )
+            neighbor_list = system.get_neighbor_list(known_neighbor_lists[0])
+            nl_values = neighbor_list.samples.values
+            franken_data = Configuration(
+                atom_pos=system.positions,
+                atomic_numbers=system.types,
+                natoms=torch.tensor(len(system.types)).view(1),
+                pbc=system.pbc,
+                cell=system.cell,
+                shifts=nl_values[:, 2:],
+                edge_index=nl_values[:, :2],
+            )
+            # Don't compute_forces. This will be done in the metatomic calculator.
+            # in theory we could set the `explicit_gradients` capability in the model
+            # but it doesn't seem to be actually used anywhere in the calculators (which
+            # rely on performing autograd themselves)
+            sys_energy, _ = self.model(franken_data, compute_forces=False)
+            energy[i] += sys_energy
+
+        # add metadata to the output
+        block = TensorBlock(
+            values=energy,
+            samples=Labels("system", torch.arange(len(systems)).reshape(-1, 1)),
+            components=[],
+            properties=Labels("energy", torch.tensor([[0]])),
+        )
+        return {
+            "energy": TensorMap(keys=Labels("_", torch.tensor([[0]])), blocks=[block])
+        }
+
+
+def create_metatomic(
+    model_path: str, rf_weight_id: int | None, dtype: torch.dtype
+) -> str:
+    """Compile a franken model into a metatomic model wrapper
+
+    Args:
+        model_path (str):
+            path to the franken model checkpoint.
+        rf_weight_id (int | None):
+            ID of the random feature weights. Can generally be left to ``None`` unless
+            the checkpoint contains multiple trained models.
+
+    Returns:
+        str: the path where the metatomic model was saved to.
+    """
+    franken_model = FrankenPotential.load(
+        model_path,
+        map_location=torch.device("cpu"),
+        rf_weight_id=rf_weight_id,
+    )
+    if not isinstance(franken_model.gnn, MetatomicModelWrapper):
+        raise NotImplementedError(
+            f"GNN underlying the franken model ({franken_model.gnn_config.path_or_id}) is not compatible with Metatomic."
+        )
+    franken_model = franken_model.to(device="cpu", dtype=dtype)
+    mta_wrapper = MetatomicFrankenWrapper(franken_model)
+
+    base_metadata = getattr(franken_model.gnn, "metadata", {})
+    metadata = ModelMetadata(
+        name="franken",
+        description="Franken model wrapping a pretrained deep-learning atomistic potential",
+        authors=["Giacomo Meanti"],
+        references={
+            "model": ["https://arxiv.org/abs/2505.05652"],
+        }
+        | base_metadata,
+    )
+    outputs = {
+        "energy": ModelOutput(quantity="energy", unit="eV", per_atom=False),
+    }
+    capabilities = ModelCapabilities(
+        outputs=outputs,
+        atomic_types=franken_model.gnn.supported_atomic_types().tolist(),
+        interaction_range=franken_model.gnn.cutoff_radius()
+        * franken_model.gnn.num_interaction_layers(),
+        length_unit="angstrom",
+        supported_devices=["cpu", "gpu"],
+        dtype="float32" if dtype == torch.float32 else "float64",
+    )
+    wrapper = AtomisticModel(
+        mta_wrapper.eval(),  # pyright: ignore[reportArgumentType]
+        metadata,
+        capabilities,
+    )
+    save_path = f"{os.path.splitext(model_path)[0]}-metatomic.pt"
+    print(f"Saving metatomic model to '{save_path}'")
+    wrapper.save(save_path)  # pyright: ignore[reportCallIssue]
+    return save_path
+
+
+def build_arg_parser():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description="Convert a franken model to be able to use it with LAMMPS",
+    )
+    parser.add_argument(
+        "--model_path",
+        type=str,
+        help="Path to the model to be converted to LAMMPS",
+    )
+    parser.add_argument(
+        "--rf_weight_id",
+        type=int,
+        help="Head of the model to be converted to LAMMPS",
+        default=None,
+    )
+    parser.add_argument(
+        "--dtype",
+        type=str,
+        choices=["float32", "float64"],
+        help="Data-type of the model which is being converted",
+        required=True,
+    )
+    return parser
+
+
+def create_metatomic_calc_cli():
+    parser = build_arg_parser()
+    args = parser.parse_args()
+    dtype = torch.float32 if args.dtype == "float32" else torch.float64
+    create_metatomic(args.model_path, args.rf_weight_id, dtype)
+
+
+if __name__ == "__main__":
+    create_metatomic_calc_cli()
+
+
+# For sphinx docs
+get_parser_fn = lambda: build_arg_parser()  # noqa: E731
---
+++ tests/test_metatomic_calc.py
@@ -0,0 +1,167 @@
+"""
+Test the model conversion to LAMMPS (essentially testing torch-scriptability, not LAMMPS directly)
+"""
+
+import os
+import pytest
+import ase
+import ase.md.velocitydistribution
+import ase.build
+import ase.units
+import numpy as np
+import torch
+from metatomic.torch import load_atomistic_model
+from metatomic.torch.ase_calculator import MetatomicCalculator
+
+from franken.backbones.wrappers.common_patches import unpatch_e3nn
+from franken.calculators.metatomic_calc import create_metatomic
+from franken.config import BackboneConfig, GaussianRFConfig, MultiscaleGaussianRFConfig
+from franken.data import BaseAtomsDataset
+from franken.rf.model import FrankenPotential
+from franken.rf.scaler import Statistics
+from franken.utils.misc import garbage_collection_cuda
+from franken.datasets.registry import DATASET_REGISTRY
+from .conftest import DEVICES
+from .utils import are_dicts_close, cleanup_dir, create_temp_dir
+
+
+RF_PARAMETRIZE = [
+    GaussianRFConfig(num_random_features=128, length_scale=1.0),
+    MultiscaleGaussianRFConfig(num_random_features=128),
+]
+
+
+@pytest.mark.parametrize("rf_cfg", RF_PARAMETRIZE)
+@pytest.mark.parametrize("device", DEVICES)
+@pytest.mark.parametrize("backbone", [("pet", "PET_OMat/xs_1.0"), ("mace", "mace_mp/small")])
+def test_preserves_info(rf_cfg, device, backbone):
+    """Test for checking save and load methods of FrankenPotential"""
+    unpatch_e3nn()  # needed in case some previous test ran the patching code (for MACE)
+    gnn_cfg = BackboneConfig.from_ckpt(
+        dict(family=backbone[0], path_or_id=backbone[1])
+    )
+
+    dtype = torch.float32
+    try:
+        # Step 1: Create a temporary directory for saving the model
+        temp_dir = create_temp_dir()
+
+        data_path = DATASET_REGISTRY.get_path("test", "test", None, False)
+        dataset = BaseAtomsDataset.from_path(
+            data_path=data_path,
+            split="train",
+            gnn_config=gnn_cfg,
+        )
+        model = FrankenPotential(
+            gnn_config=gnn_cfg,
+            rf_config=rf_cfg,
+            scale_by_Z=True,
+            num_species=dataset.num_species,
+        ).to(device, dtype=dtype)
+        with torch.no_grad():
+            gnn_features_stats = Statistics()
+            for data, _ in dataset:  # type: ignore
+                data = data.to(device=device, dtype=dtype)
+                gnn_features = model.gnn.descriptors(data)
+                gnn_features_stats.update(
+                    gnn_features, atomic_numbers=data.atomic_numbers
+                )
+
+            model.input_scaler.set_from_statistics(gnn_features_stats)
+            garbage_collection_cuda()
+
+        # Step 2: Save the model to the temporary directory
+        model_save_path = os.path.join(temp_dir, "model_checkpoint.pth")
+        model.save(model_save_path)
+
+        # Step 3: Run create_metatomic
+        comp_model_path = create_metatomic(model_path=model_save_path, rf_weight_id=None, dtype=dtype)
+
+        # Step 4: Load saved model
+        mta_model = load_atomistic_model(comp_model_path)
+        mta_franken = mta_model.module.model
+
+        # Step 5: Compare rf.state_dict between the original and loaded models
+        assert are_dicts_close(
+            model.rf.state_dict(), mta_franken.rf.state_dict(), verbose=True
+        ), "The rf.state_dict() of the loaded model does not match the original model."
+
+        assert are_dicts_close(
+            model.input_scaler.state_dict(),
+            mta_franken.input_scaler.state_dict(),
+            verbose=True,
+        ), "The input_scaler.state_dict() of the loaded model does not match the original model."
+
+        assert are_dicts_close(
+            model.energy_shift.state_dict(),
+            mta_franken.energy_shift.state_dict(),
+            verbose=True,
+        ), "The energy_shift.state_dict() of the loaded model does not match the original model."
+    finally:
+        if temp_dir is not None:
+            cleanup_dir(temp_dir)
+
+
+@pytest.mark.parametrize("rf_cfg", RF_PARAMETRIZE)
+@pytest.mark.parametrize("device", DEVICES)
+@pytest.mark.parametrize("dtype", [torch.float32, torch.float64])
+@pytest.mark.parametrize("backbone", [("pet", "PET_OMat/xs_1.0"), ("mace", "mace_mp/small")])
+def test_calc_for_asemd(rf_cfg, device, dtype, backbone):
+    unpatch_e3nn()  # needed in case some previous test ran the patching code
+    gnn_cfg = BackboneConfig.from_ckpt(
+        dict(family=backbone[0], path_or_id=backbone[1])
+    )
+    temp_dir = None
+    try:
+        # Step 1: Create a temporary directory for saving the model
+        temp_dir = create_temp_dir()
+
+        data_path = DATASET_REGISTRY.get_path("test", "test", None, False)
+        dataset = BaseAtomsDataset.from_path(
+            data_path=data_path,
+            split="train",
+            gnn_config=gnn_cfg,
+        )
+        model = FrankenPotential(
+            gnn_config=gnn_cfg,
+            rf_config=rf_cfg,
+            scale_by_Z=True,
+            num_species=dataset.num_species,
+        ).to(device, dtype=dtype)
+        with torch.no_grad():
+            gnn_features_stats = Statistics()
+            for data, _ in dataset:  # type: ignore
+                data = data.to(device=device, dtype=dtype)
+                gnn_features = model.gnn.descriptors(data)
+                gnn_features_stats.update(
+                    gnn_features, atomic_numbers=data.atomic_numbers
+                )
+
+            model.input_scaler.set_from_statistics(gnn_features_stats)
+            garbage_collection_cuda()
+
+        # Step 2: Save the model to the temporary directory
+        model_save_path = os.path.join(temp_dir, "model_checkpoint.pth")
+        model.save(model_save_path)
+
+        # Step 3: Run create_metatomic
+        comp_model_path = create_metatomic(model_path=model_save_path, rf_weight_id=None, dtype=dtype)
+
+        # Step 4: Crease ASE structure for MD
+        primitive = ase.build.bulk(name="C", crystalstructure="diamond", a=3.567)
+        atoms = ase.build.make_supercell(primitive, 3 * np.eye(3))
+        ase.md.velocitydistribution.MaxwellBoltzmannDistribution(atoms, temperature_K=300)
+        atoms.calc = MetatomicCalculator(comp_model_path)
+        integrator = ase.md.Langevin(
+            atoms,
+            timestep=1.0 * ase.units.fs,
+            temperature_K=300,
+            friction=0.1 / ase.units.fs,
+        )
+        # run short simulation just to check that things don't blow up!
+        for _ in range(5):
+            integrator.run(1)
+            print(atoms.get_total_energy())
+    finally:
+        if temp_dir is not None:
+            cleanup_dir(temp_dir)
