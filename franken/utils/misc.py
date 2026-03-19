86e9918a598dd178a16dc5916e98294913283149
Giacomo Meanti <gmeanti@inria.fr>
2026-03-19 18:03:13 +0100

    Deprecated positional arg

--- franken/utils/misc.py
+++ franken/utils/misc.py
@@ -212,7 +212,7 @@ def get_device_name(device: str | None) -> str:
                 all_info = subprocess.check_output(command, shell=True).decode().strip()
                 for line in all_info.split("\n"):
                     if "model name" in line:
-                        return re.sub(".*model name.*:", "", line, 1)
+                        return re.sub(".*model name.*:", "", line, count=1)
             return ""
 
         hardware = get_processor_name()
