b216f1dd947ca4f46a1f4030df911b5127a3be03
Giacomo Meanti <gmeanti@inria.fr>
2026-03-21 10:16:46 +0100

    Fix

--- franken/backbones/wrappers/mace_wrap.py
+++ franken/backbones/wrappers/mace_wrap.py
@@ -398,9 +398,7 @@ class FrankenMACE(torch.nn.Module):
             natoms=partial_config.natoms,
             unit_shifts=torch.from_numpy(unit_shifts).to(dtype=dtype, device=device),
             shifts=torch.from_numpy(shifts).to(dtype=dtype, device=device),
-            edge_index=torch.from_numpy(edge_index)
-            .to(dtype=dtype, device=device)
-            .transpose(0, 1),
+            edge_index=torch.from_numpy(edge_index).to(device=device).transpose(0, 1),
         )
 
     @staticmethod
