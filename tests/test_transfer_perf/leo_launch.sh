2d66a188038de476dddd615f3d4d45e37e5b8d57
Giacomo Meanti <giacomo.meanti@unige.it>
2026-03-26 19:11:31 +0100

    Add launcher

---
+++ tests/test_transfer_perf/leo_launch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --account=IscrB_ProAmmo
+#SBATCH --time 24:00:00             # format: HH:MM:SS
+#SBATCH --nodes=1                   # node
+#SBATCH --ntasks-per-node=1         # tasks out of 32
+#SBATCH --gres=gpu:1                # gpus per node out of 4
+#SBATCH --cpus-per-task=10
+#SBATCH -p boost_usr_prod
+##SBATCH --qos=boost_qos_dbg 
+############################
+
+. ~/.bashrc
+
+mamba activate franken-dev
+cd /leonardo/home/userexternal/gmeanti0/franken
+
+PYTHONPATH='.' python tests/test_transfer_perf/test_water.py --db-path=tests/test_transfer_perf/results_2603.pkl
