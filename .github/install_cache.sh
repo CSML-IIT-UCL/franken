84f5e8150df1c3a4e2d720b2b320e90007e376d9
Giacomo Meanti <giacomo.meanti@unige.it>
2026-07-21 11:14:22 +0200

    Upd

--- .github/install_cache.sh
+++ .github/install_cache.sh
@@ -135,8 +135,10 @@ gen_pip_reqs() {
     REQ_FILE=".github/reqs.txt"
     # PyTorch
     echo "-r torch_reqs.txt" >> $REQ_FILE
-    echo "--index-url https://download.pytorch.org/whl/cpu" >> "./github/torch_reqs.txt"
-    echo "torch==${PYTORCH_VERSION}" >> "./github/torch_reqs.txt"
+    cat > ".github/torch_reqs.txt" << 'EOF'
+--index-url https://download.pytorch.org/whl/cpu"
+torch==${PYTORCH_VERSION}"
+EOF
     # Torch Geometric
     echo "torch_geometric" >> $REQ_FILE
     echo "-r pyg_reqs.txt" >> $REQ_FILE
