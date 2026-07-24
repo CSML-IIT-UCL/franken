Fix gradient propagation

franken/rf/les_model.py
@@ -142,7 +142,7 @@ class LESFrankenPotential(FrankenPotential):
         natoms = data.natoms.to(dtype=weights.dtype).view(-1)  # [N]
         rff_energies = torch.matmul(random_features, weights.T).T  # [M, N]
         rff_energies = natoms[None, :] * rff_energies
-        les_energies = self.les(gnn_descriptors, data)
+        les_energies = self.les(gnn_descriptors, atom_pos, data)
         energies = rff_energies + les_energies
 
         return energies.sum(1), energies
@@ -154,36 +154,46 @@ class LESFrankenPotential(FrankenPotential):
         data: Configuration,
     ):
         gnn_descriptors = self.descriptors(atom_pos, displacement, data)
-        les_energies = self.les(gnn_descriptors, data)
-        return les_energies.sum(1), les_energies
+        les_energies = self.les(gnn_descriptors, atom_pos, data)
+        return les_energies, les_energies
 
     def _predict(  # pyright: ignore[reportIncompatibleMethodOverride]
         self,
         weights: torch.Tensor | None,
         data: Configuration,
         targets: list[str],
+        is_training: bool,
     ) -> dict[str, torch.Tensor]:
         if weights is None:
             weights = self.rf.weights
         compute_force = franken.data.base.FORCES_TARGET_KEY in targets
         compute_stress = franken.data.base.STRESS_TARGET_KEY in targets
         if compute_stress:
-            return forces_stress_bwdad(data, fn=self._energy_aux, weights=weights)
+            return forces_stress_bwdad(
+                data, fn=self._energy_aux, is_training=is_training, weights=weights
+            )
         elif compute_force:
-            return forces_bwdad(data, fn=self._energy_aux, weights=weights)
+            return forces_bwdad(
+                data, fn=self._energy_aux, is_training=is_training, weights=weights
+            )
         else:
             _, energy = self._energy_aux(data.atom_pos, None, data, weights)  # [M, N]
             return {franken.data.base.ENERGY_TARGET_KEY: energy}
 
     def predict_les(
-        self, data: Configuration, targets: list[str]
+        self,
+        data: Configuration,
+        targets: list[str],
+        is_training: bool = False,
     ) -> dict[str, torch.Tensor]:
         compute_force = franken.data.base.FORCES_TARGET_KEY in targets
         compute_stress = franken.data.base.STRESS_TARGET_KEY in targets
         if compute_stress:
-            return forces_stress_bwdad(data, fn=self._les_energy_aux)
+            return forces_stress_bwdad(
+                data, fn=self._les_energy_aux, is_training=is_training
+            )
         elif compute_force:
-            return forces_bwdad(data, fn=self._les_energy_aux)
+            return forces_bwdad(data, fn=self._les_energy_aux, is_training=is_training)
         else:
             _, energy = self._les_energy_aux(data.atom_pos, None, data)  # [M, N]
             return {franken.data.base.ENERGY_TARGET_KEY: energy}
@@ -195,6 +205,7 @@ class LESFrankenPotential(FrankenPotential):
         weights: torch.Tensor | None = None,
         differential_mode: str = "torch.autograd",
         add_energy_shift: bool = True,
+        is_training: bool = False,
     ) -> dict[str, torch.Tensor]:
         """Infer energy, forces and other quantities for an atomic system with a learned RF model.
 
@@ -221,7 +232,7 @@ class LESFrankenPotential(FrankenPotential):
             Use `"torch.autograd"` if the model has been processed by :code:`torch.jit.script`.
         """
         natoms = torch.atleast_1d(data.natoms)
-        out = self._predict(weights, data, targets)
+        out = self._predict(weights, data, targets, is_training=is_training)
 
         if add_energy_shift and franken.data.base.ENERGY_TARGET_KEY in targets:
             out[franken.data.base.ENERGY_TARGET_KEY] = out[

franken/rf/model.py
@@ -245,7 +245,7 @@ class FrankenPotential(torch.nn.Module):
                 data.atom_pos, displacement, data, weights=weights
             )
         forces, stress = _forces_stress_bwdad_helper(
-            energies=energy, displacement=displacement, data=data
+            energies=energy, displacement=displacement, data=data, is_training=False
         )
         return {
             franken.data.base.FORCES_TARGET_KEY: forces.detach(),
@@ -261,7 +261,7 @@ class FrankenPotential(torch.nn.Module):
             _, energy = self._feature_map_aux(data.atom_pos, None, data)
         else:
             _, energy = self._energy_aux(data.atom_pos, None, data, weights=weights)
-        forces = _forces_bwdad_helper(energy, data)
+        forces = _forces_bwdad_helper(energy, data, is_training=False)
         return {
             franken.data.base.FORCES_TARGET_KEY: forces.detach(),
             franken.data.base.ENERGY_TARGET_KEY: energy.detach(),

franken/utils/derivatives.py
@@ -24,23 +24,32 @@ useful for when there is a single (or few) outputs and many inputs
 def forces_bwdad(
     data: Configuration,
     fn: Callable,
+    is_training: bool = False,
     **extra_args: torch.Tensor,
 ):
     data.atom_pos.requires_grad_(True)
-    _, energy = fn(data.atom_pos, displacement=None, data=data, **extra_args)
     with torch.enable_grad():
-        forces = _forces_bwdad_helper(energies=energy, data=data)
+        _, energy = fn(data.atom_pos, displacement=None, data=data, **extra_args)
+        forces = _forces_bwdad_helper(
+            energies=energy, data=data, is_training=is_training
+        )
+    if not is_training:
+        forces = forces.detach()
+        energy = energy.detach()
     return {
-        franken.data.base.FORCES_TARGET_KEY: forces.detach(),
-        franken.data.base.ENERGY_TARGET_KEY: energy.detach(),
+        franken.data.base.FORCES_TARGET_KEY: forces,
+        franken.data.base.ENERGY_TARGET_KEY: energy,
     }
 
 
-def _forces_bwdad_helper(energies: torch.Tensor, data: Configuration):
+def _forces_bwdad_helper(
+    energies: torch.Tensor, data: Configuration, is_training: bool
+):
     n_sols = energies.shape[0]
     n_atoms = data.atom_pos.shape[0]
     force_lst = []
     for i in range(n_sols):  # each model (M) independently
+        retain_graph = True if is_training else i < n_sols - 1
         cur_energy: torch.Tensor = energies[i]
         # NOTE: complex type annotation required by jit.script
         grad_out: List[Optional[torch.Tensor]] = [torch.ones_like(cur_energy)]
@@ -48,7 +57,7 @@ def _forces_bwdad_helper(energies: torch.Tensor, data: Configuration):
             outputs=[cur_energy],
             inputs=[data.atom_pos],
             grad_outputs=grad_out,  # type: ignore
-            retain_graph=i < n_sols - 1,
+            retain_graph=retain_graph,
         )
         grad = grads[0]
         assert grad is not None
@@ -60,6 +69,7 @@ def _forces_bwdad_helper(energies: torch.Tensor, data: Configuration):
 def forces_stress_bwdad(
     data: Configuration,
     fn: Callable,
+    is_training: bool = False,
     **extra_args: torch.Tensor,
 ):
     n_systems = data.natoms.numel()
@@ -73,18 +83,28 @@ def forces_stress_bwdad(
         _, energy = fn(
             data.atom_pos, displacement=displacement, data=data, **extra_args
         )
-    forces, stress = _forces_stress_bwdad_helper(
-        energies=energy, displacement=displacement, data=data
-    )
+        forces, stress = _forces_stress_bwdad_helper(
+            energies=energy,
+            displacement=displacement,
+            data=data,
+            is_training=is_training,
+        )
+    if not is_training:
+        stress = stress.detach()
+        forces = forces.detach()
+        energy = energy.detach()
     return {
-        franken.data.base.FORCES_TARGET_KEY: forces.detach(),
-        franken.data.base.STRESS_TARGET_KEY: stress.detach(),
-        franken.data.base.ENERGY_TARGET_KEY: energy.detach(),
+        franken.data.base.FORCES_TARGET_KEY: forces,
+        franken.data.base.STRESS_TARGET_KEY: stress,
+        franken.data.base.ENERGY_TARGET_KEY: energy,
     }
 
 
 def _forces_stress_bwdad_helper(
-    energies: torch.Tensor, displacement: torch.Tensor, data: Configuration
+    energies: torch.Tensor,
+    displacement: torch.Tensor,
+    data: Configuration,
+    is_training: bool,
 ):
     """
     energies: [n_sols, n_systems]
@@ -96,6 +116,7 @@ def _forces_stress_bwdad_helper(
 
     force_lst, virial_lst = [], []
     for i in range(n_sols):  # each model (M) independently
+        retain_graph = True if is_training else i < n_sols - 1
         cur_energy: torch.Tensor = energies[i]
         # NOTE: complex type annotation required by jit.script
         grad_out: List[Optional[torch.Tensor]] = [torch.ones_like(cur_energy)]
@@ -103,7 +124,7 @@ def _forces_stress_bwdad_helper(
             outputs=[cur_energy],
             inputs=[data.atom_pos, displacement],
             grad_outputs=grad_out,  # type: ignore
-            retain_graph=i < n_sols - 1,
+            retain_graph=retain_graph,
         )
         g0 = grads[0]
         assert g0 is not None